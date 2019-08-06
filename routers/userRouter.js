c7f75117bc997027e348c770bdbee5172557437b
adauren <frankcawperwood818@gmail.com>
2019-08-06 18:43:54 +0600

    User and video controllers basic setup

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,22 +1,19 @@
 import express from "express";
 import routes from "../routes";
-
+import {
+  users,
+  editProfile,
+  userDetail,
+  changePassword
+} from "../controllers/userController";
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {
-  res.send("Users");
-});
+userRouter.get("/", users);
 
-userRouter.get(routes.videoDetail, (req, res) => {
-  res.send("User detail");
-});
+userRouter.get(routes.userDetail, userDetail);
 
-userRouter.get(routes.editVideo, (req, res) => {
-  res.send("User edit profile");
-});
+userRouter.get(routes.editProfile, editProfile);
 
-userRouter.get(routes.changePassword, (req, res) => {
-  res.send("User change password");
-});
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,26 +1,22 @@
 import express from "express";
 import routes from "../routes";
-
+import {
+  videos,
+  videoDetail,
+  videoUpload,
+  editVideo,
+  deleteVideo
+} from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get("/", (req, res) => {
-  res.send("Videos");
-});
+videoRouter.get("/", videos);
 
-videoRouter.get(routes.videoUpload, (req, res) => {
-  res.send("Video upload");
-});
+videoRouter.get(routes.videoUpload, videoUpload);
 
-videoRouter.get(routes.videoDetail, (req, res) => {
-  res.send("Video detail");
-});
+videoRouter.get(routes.videoDetail, videoDetail);
 
-videoRouter.get(routes.editVideo, (req, res) => {
-  res.send("Video edit");
-});
+videoRouter.get(routes.editVideo, editVideo);
 
-videoRouter.get(routes.deleteVideo, (req, res) => {
-  res.send("Video delete");
-});
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
