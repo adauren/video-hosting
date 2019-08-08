244671aba52d17508c41948e8f0db693dc1a5b44
adauren <frankcawperwood818@gmail.com>
2019-08-08 12:27:49 +0600

    Basic view setup

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const videoUpload = (req, res) => res.send("Video Upload");
-export const videoDetail = (req, res) => res.send("Video detail");
-export const editVideo = (req, res) => res.send("Edit video");
-export const deleteVideo = (req, res) => res.send("Delete video");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
+export const videos = (req, res) => res.render("videos");
+export const videoUpload = (req, res) => res.render("videoUpload");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
